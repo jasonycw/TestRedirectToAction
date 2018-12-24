07dd4ba5f9feaa8d6b5cb2c0d66f1c77cf991585
Jason Yu <jason_yu@executivecentre.com>
2018-12-24 10:55:16 +0800

    Push test project

---
+++ TestApi/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace TestApi.Controllers
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    [ApiVersion("1.0")]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get() => RedirectToAction("Get", "Values");
+    }
+}
